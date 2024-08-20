c607e69a7ac32ba2a890aff7dd7c7c764e7b49a5
Yuliia-Tyzhai <164753475+Yuliia-Tyzhai@users.noreply.github.com>
2024-08-20 22:28:17 +0000

    Deploying to gh-pages from @ Yuliia-Tyzhai/goit-js-hw-10@f285c6954f2d5b635a74610b80ceaf84fb275830 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-a0b12faf.js";import{i}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",t=>{t.preventDefault();const s=Number(t.target.delay.value),o=t.target.state.value;new Promise((e,r)=>{setTimeout(()=>{o==="fulfilled"?e(s):r(s)},s)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`,position:"topRight"})})});
+//# sourceMappingURL=commonHelpers2.js.map
